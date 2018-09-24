295802e087140c9233a0aa83be818687e720638f
Reece_Collett <rcollett@spartaglobal.com>
2018-09-24 16:31:19 +0100

    Added updating of packages, TESTED

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+apt_update "update_sources" do
+  action :update
+end
+
 # Include the nodejs recipe
 include_recipe("nodejs")
 
@@ -18,14 +22,17 @@ end
 
 template "/etc/nginx/sites-available/proxy.conf" do
   source "proxy.conf.erb"
+  notifies(:restart, "service[nginx]")
 end
 
 link "/etc/nginx/sites-enabled/proxy.conf" do
   to "/etc/nginx/sites-available/proxy.conf"
+  notifies(:restart, "service[nginx]")
 end
 
 link "/etc/nginx/sites-enabled/default" do
   action :delete
+  notifies(:restart, "service[nginx]")
 end
 
 # Starts nginx
